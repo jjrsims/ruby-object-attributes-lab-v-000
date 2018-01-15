be12010f407e8d70f77bf994dbb6e002935dba1c
Johannah Sims <jjrsims@gmail.com>
2018-01-15 19:46:54 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,26 +1,19 @@
 class Person
 
-  def initialize(name)
+  def name=(name)
     @name = name
   end
-
+  
   def name
     @name
   end
 
-  def name=(new_name)
-    @name = new_name
+  def job=(job)
+    @job = job
   end
 
  def job
    @job
  end
 
- def job=(new_job)
-   @job = new_job
- end
-
 end
-
-beyonce = Person.new("Beyonce")
-beyonce.name
